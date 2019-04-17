ded85109829b7e4f8f6e9de07ebe6e150cf9c62e
朱晓明 <710535190@qq.com>
2019-04-17 17:56:24 +0800

    测试提交

---
+++ src/main/java/com/itcast/pojo/User.java
@@ -0,0 +1,13 @@
+package com.itcast.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author xiaoming
+ * @create 2019-04-17-17:53
+ */
+public class User {
+    private Integer id;
+    private Integer name;
+}
